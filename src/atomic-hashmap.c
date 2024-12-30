48ab590fc6b4526e199baadc4e3ddcf4083c03cd
KittenLord <vanya.braslavets@gmail.com>
2024-12-30 22:00:46 +0200

    implementing basic config (the only feature i want is custom save file really)

---
+++ src/atomic-hashmap.c
@@ -0,0 +1,188 @@
+#ifndef KITTENLORD_ATOMIC_HASHMAP__
+#define KITTENLORD_ATOMIC_HASHMAP__
+
+#include <stdlib.h>
+#include <stdio.h> 
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+// TODO: obviously needs rebalancing functionality, but ill add it later lol
+
+struct AtomicHashmapNode {
+    uint8_t                     *key;
+    size_t                      keyLen;
+
+    uint8_t                     *data;
+    size_t                      dataLen;
+
+    struct AtomicHashmapNode    *next;
+};
+
+struct AtomicHashmap {
+    _Atomic bool                locked;
+
+    size_t                      capacity;
+
+    size_t                      tyA;
+    size_t                      tyB;
+    
+    size_t                      maxChain;
+    size_t                      count;
+
+    struct AtomicHashmapNode    **nodes;
+};
+
+#define HM_INITCAPACITY 32
+#define createHM_SX(ty) createHMAll(HM_INITCAPACITY, sizeof(ty), 0)
+#define createHM_XS(ty) createHMAll(HM_INITCAPACITY, 0, sizeof(ty))
+#define createHM_SS(tyA, tyB) createHMAll(HM_INITCAPACITY, sizeof(tyA), sizeof(tyB))
+#define createHM() createHMAll(HM_INITCAPACITY, 0, 0)
+#define createHMAll(cap, a, b) ((struct AtomicHashmap){ \
+    .locked = false, \
+    .capacity = cap, \
+    .maxChain = 0, \
+    .count = 0, \
+    .tyA = a, \
+    .tyB = b, \
+    .nodes = calloc(cap, sizeof(struct AtomicHashmapNode *)) \
+})
+
+// Please be responsible :D
+void acquireLock(struct AtomicHashmap *hm);
+void releaseLock(struct AtomicHashmap *hm);
+
+#define getHM_SX(hm, key, dataLen) getHM(hm, (uint8_t *)(key), (hm)->tyA, dataLen)
+#define getHM_XS(hm, key, keyLen) getHM(hm, (uint8_t *)(key), keyLen, NULL)
+#define getHM_SS(hm, key) getHM(hm, (uint8_t *)(key), (hm)->tyA, NULL)
+uint8_t *getHM(struct AtomicHashmap *hm, uint8_t *key, size_t keyLen, size_t *dataLen);
+
+#define setHM_SX(hm, key, data, dataLen) setHM(hm, (uint8_t *)(key), (hm)->tyA, (uint8_t *)(data), dataLen)
+#define setHM_XS(hm, key, keyLen, data) setHM(hm, (uint8_t *)(key), keyLen, (uint8_t *)(data), (hm)->tyB)
+#define setHM_SS(hm, key, data) setHM(hm, (uint8_t *)(key), (hm)->tyA, (uint8_t *)(data), (hm)->tyB)
+#define setHM_LL(hm, key, data) setHM(hm, key, strlen(key), data, strlen(data))
+void setHM(struct AtomicHashmap *hm, uint8_t *key, size_t keyLen, uint8_t *data, size_t dataLen);
+
+bool removeHM(struct AtomicHashmap *hm, uint8_t *key, size_t keyLen);
+
+// https://github.com/fabiogaluppo/fnv/blob/main/fnv64.hpp
+// from here
+uint64_t fnv64hash(uint8_t *data, size_t len) {
+    uint64_t h = 0xcbf29ce484222325ULL;
+    while (len--) {
+        h += (h << 1) + (h << 4) + (h << 5) + (h << 7) + (h << 8) + (h << 40);
+        h ^= (uint64_t)(*data++);
+    }
+    return h;
+}
+// to here
+
+void acquireLock(struct AtomicHashmap *hm) { 
+    while(hm->locked);  
+    hm->locked = true;
+}
+
+void releaseLock(struct AtomicHashmap *hm) {
+    hm->locked = false;
+}
+
+void trySetMaxChain(struct AtomicHashmap *hm, size_t value) {
+    if(hm->maxChain < value) hm->maxChain = value;
+}
+
+void __freeHMNode(struct AtomicHashmapNode *node) {
+    free(node->key);
+    free(node->data);
+    free(node);
+}
+
+bool __nodeMatchesKey(struct AtomicHashmapNode *node, uint8_t *key, size_t keyLen) {
+    return node->keyLen == keyLen && !memcmp(node->key, key, keyLen);
+}
+
+bool removeHM(struct AtomicHashmap *hm, uint8_t *key, size_t keyLen) {
+    size_t hash = fnv64hash(key, keyLen);
+    size_t index = hash % hm->capacity;
+
+    struct AtomicHashmapNode *node = hm->nodes[index];
+    if(__nodeMatchesKey(node, key, keyLen)) {
+        hm->nodes[index] = node->next;
+        __freeHMNode(node);
+        hm->count--;
+        return true;
+    }
+
+    while(node->next) {
+        struct AtomicHashmapNode *next = node->next;
+        if(__nodeMatchesKey(next, key, keyLen)) {
+            node->next = next->next;
+            __freeHMNode(next);
+            hm->count--;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+uint8_t *getHM(struct AtomicHashmap *hm, uint8_t *key, size_t keyLen, size_t *dataLen) {
+    size_t hash = fnv64hash(key, keyLen);
+    size_t index = hash % hm->capacity;
+
+    struct AtomicHashmapNode *node = hm->nodes[index];
+    while(node) {
+        if(__nodeMatchesKey(node, key, keyLen)) {
+            if(dataLen) *dataLen = node->dataLen;
+            return node->data;
+        }
+
+        node = node->next;
+    }
+
+    return NULL;
+}
+
+void setHM(struct AtomicHashmap *hm, uint8_t *key, size_t keyLen, uint8_t *data, size_t dataLen) {
+    size_t hash = fnv64hash(key, keyLen);
+    size_t index = hash % hm->capacity;
+
+    size_t maxChain = 1;
+    struct AtomicHashmapNode **insertNode = &(hm->nodes[index]);
+
+    // definitely doesnt exist
+    if(!*insertNode) { goto insert; }
+
+    do {
+        struct AtomicHashmapNode *node = *insertNode;
+        if(__nodeMatchesKey(node, key, keyLen)) {
+            free(node->data);
+            node->data = malloc(dataLen);
+            node->dataLen = dataLen;
+            memcpy(node->data, data, dataLen);
+            return;
+        }
+
+        insertNode = &(node->next);
+        maxChain++;
+    }
+    while(*insertNode);
+    goto insert;
+
+insert:
+    struct AtomicHashmapNode *node = malloc(sizeof(struct AtomicHashmapNode));
+    *node = (struct AtomicHashmapNode){
+        .key = malloc(keyLen),
+        .keyLen = keyLen,
+        .data = malloc(dataLen),
+        .dataLen = dataLen,
+        .next = NULL
+    };
+    memcpy(node->key, key, keyLen);
+    memcpy(node->data, data, dataLen);
+    *insertNode = node;
+    trySetMaxChain(hm, maxChain);
+    hm->count++;
+}
+
+#else
+#endif
--- src/main.c
+++ src/main.c
@@ -1,13 +1,155 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <errno.h>
 
 #include "planitems.c"
 #include "interactive.c"
+#include "atomic-hashmap.c"
 
 #define SUB_INTERACTIVE "interactive"
 
+#define STREAM_STR 1
+#define STREAM_FILE 2
+typedef struct {
+    char type;
+    union {
+        struct {
+            FILE *file;
+        };
+        struct {
+            char *ptr;
+            size_t length;
+            size_t index;
+        };
+    };
+} stream;
+
+#define stream_str(str, len) ((stream){ .type = STREAM_STR, .ptr = str, .length = len, .index = 0 })
+#define stream_strlen(str) ((stream){ .type = STREAM_STR, .ptr = str, .length = strlen(str), .index = 0 })
+#define stream_file(f) ((stream){ .type = STREAM_FILE, .file = f })
+
+char readStream(stream *s) {
+    if(s->type == STREAM_FILE) {
+        if(!s->file) return EOF;
+        char c;
+        int r = fread(&c, sizeof(char), 1, s->file);
+        if(r <= 0) return EOF;
+        return c;
+    }
+    else if(s->type == STREAM_STR) {
+        if(!s->ptr) return EOF;
+        if(s->index >= s->length) return EOF;
+        return s->ptr[s->index++];
+    }
+}
+
+char *strcatnew(char *a, char *b, bool freeA) {
+    size_t alen = strlen(a);
+    size_t blen = strlen(b);
+    char *result = calloc(sizeof(char), alen + blen + 1); // null terminator
+    memcpy(result, a, alen);
+    memcpy(result + alen, b, blen);
+    if(freeA) free(a);
+    return result;
+}
+
+char *getPathWithHome(char *path) {
+    if(!path) return NULL;
+    if(*path != '~') return path;
+    
+    return strcatnew(getenv("HOME"), path + 1, false);
+}
+
+// false on failure
+bool ensureDirectoryExists(char *path) {
+    return mkdir(path, 0700) != -1 || errno == 17; // errno 17 : Already exists
+}
+
+void parseConfig(stream *s, struct AtomicHashmap *hm) {
+    char bufKey[2048] = {0}; // i really don't wanna bother doing a dynamic array here
+    char bufVal[2048] = {0};
+    char *ptr = bufKey;
+    char c;
+    while((c = readStream(s)) != EOF && c != '\n' && c != '=') {
+        *ptr++ = c;
+    }
+    
+    if(strlen(bufKey) == 0) return;
+
+    if(c == EOF) {
+        // parsed key, but there was no value
+        fprintf(stderr, "ERROR: error when parsing config, kill yourself\n");
+        return;
+    }
+
+    // if c != EOF, then '=' has been consumed, so we can parse the value
+    ptr = bufVal;
+    while((c = readStream(s)) != EOF && c != '\n' && c != ';') {
+        *ptr++ = c;
+    }
+
+    setHM_LL(hm, bufKey, bufVal);
+
+    if(c == EOF) return; // end of input
+    parseConfig(s, hm); // '\n' or ';' have already been consumed, we can just recursively parse
+}
+
+FILE *getConfig() {
+    char *configPath = getPathWithHome(getenv("XDG_CONFIG_HOME"));
+    bool result = true;
+    bool useDefaultPath = false;
+
+    // if XDG_CONFIG_HOME is not set use default
+    if(!configPath) {
+        useDefaultPath = true;
+        configPath = getenv("HOME");
+        configPath = strcatnew(configPath, "/.config", false);
+        result = ensureDirectoryExists(configPath);
+        if(!result) return NULL;
+    }
+
+    configPath = strcatnew(configPath, "/rember", useDefaultPath);
+    result = ensureDirectoryExists(configPath);
+
+    if(!result) return NULL;
+
+    configPath = strcatnew(configPath, "/config", true);
+    FILE *configFile = fopen(configPath, "r");
+
+    if(!configFile) {
+        configFile = fopen(configPath, "w");
+        if(!configFile) {}
+        else { fclose(configFile); }
+    }
+
+    free(configPath);
+
+    return configFile;
+}
+
 int main(int argc, char **argv) {
+    struct AtomicHashmap configHM = createHM();
+    FILE *configFile = getConfig();
+
+    // TODO: later provide ability to pass config via a string arg
+    stream configStream = stream_file(configFile);
+
+    parseConfig(&configStream, &configHM);
+
+    // insertHM_LL(&config, "CONFIG_PATH", "");
+
+    // size_t length;
+    // char *testKey = getHM(&configHM, "test", 4, &length);
+    // if(!testKey) {
+    //     printf("no such key\n");
+    // }
+    // else {
+    //     char *t = calloc(sizeof(char), length + 1);
+    //     memcpy(t, testKey, length);
+    //     printf("%s\n", t);
+    // }
+
     char *subcommand = SUB_INTERACTIVE;
 
     for(int i = 1; i < argc; i++) {
