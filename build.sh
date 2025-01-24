497a2a6dc940a808303a5020f6f6ef9e208dd872
KittenLord <vanya.braslavets@gmail.com>
2025-01-24 08:35:29 +0200

    build script

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 # -Wno-pointer-sign because of char* and uint8_t* distinction while trying to store strings in the hashmap (fix this whenever I implement a new hashmap)
+rm -r ./out
+mkdir ./out
 gcc ./src/main.c -o ./out/rember -ggdb -Wall -Wextra -Wno-pointer-sign && ./out/rember $@
